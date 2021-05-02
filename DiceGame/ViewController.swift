2ffe79b9c31af39a104a09946ced0ab41fe0a18b
umutgudul <umutgudul@gmail.com>
2021-05-02 21:12:58 +0300

    Initial Commit

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by UMUT GUDUL on 2.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
